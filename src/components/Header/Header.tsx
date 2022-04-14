5cd4a08b449714391756548384c59583cc356c17
Yerkhan Baigal <baigal.erhan@gmail.com>
2022-04-14 21:54:58 +0600

    remove container

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import {Burger, ColorScheme, Container, Group, Header as MantineHeader, useMantineTheme} from '@mantine/core'
+import {Burger, ColorScheme, Group, Header as MantineHeader, useMantineTheme} from '@mantine/core'
 import {useMediaQuery} from '@mantine/hooks'
 import {FC} from 'react'
 import {Login} from 'tabler-icons-react'
@@ -22,24 +22,22 @@ export const Header: FC<Props> = ({toggleColorScheme, isNavbarOpen, toggleNavbar
 
 	return (
 		<MantineHeader height={70} py='md' px={isTablet ? 'xs' : 'md'}>
-			<Container px={isTablet ? 0 : 'md'}>
-				<Group position='apart'>
-					{isTablet ? (
-						<Burger opened={isNavbarOpen} onClick={onBurgerClick} size={26} color='gray'/>
-					) : (
-						<ButtonLink path='/' px={0} className={s.logoWrapper}
-						>
-							ВГПУ
-						</ButtonLink>
-					)}
-					<Group>
-						<ButtonLink path='/auth' variant='light' rightIcon={<Login size={20}/>}>
-							Войти
-						</ButtonLink>
-						<DarkModeButton toggle={toggleColorScheme}/>
-					</Group>
+			<Group position='apart'>
+				{isTablet ? (
+					<Burger opened={isNavbarOpen} onClick={onBurgerClick} size={26} color='gray'/>
+				) : (
+					<ButtonLink path='/' px={0} className={s.logoWrapper}
+					>
+						ВГПУ
+					</ButtonLink>
+				)}
+				<Group>
+					<ButtonLink path='/auth' variant='light' rightIcon={<Login size={20}/>}>
+						Войти
+					</ButtonLink>
+					<DarkModeButton toggle={toggleColorScheme}/>
 				</Group>
-			</Container>
+			</Group>
 		</MantineHeader>
 	)
 }
